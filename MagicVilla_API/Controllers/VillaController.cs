1106c8a131dc16dd7b7efa83ff4906a51d88bc3e
Rafael Matos <120438100+Ing-Matos@users.noreply.github.com>
2023-07-30 21:56:10 -0400

    Subida de las dos primeros endpoints

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,42 @@
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class VillaController : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult<IEnumerable<VillaDto>> GetVillas()
+		{
+			if (VillaStore.villasDtos == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(VillaStore.villasDtos);
+		}
+
+		[HttpGet("{Id:int}")]
+		public ActionResult<VillaDto?> GetVilla(int? Id)
+		{
+			if (Id == 0 || Id == null)
+			{
+				return BadRequest();
+			}
+
+			var villa = VillaStore.villasDtos.FirstOrDefault(v => v.Id == Id);
+
+			if (villa == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(villa);
+		}
+	}
+}
